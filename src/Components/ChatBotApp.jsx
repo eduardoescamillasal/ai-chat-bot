268b1b55ab604eb0b39ca0146463d133149bef22
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-20 20:55:59 -0600

    more styling

--- src/Components/ChatBotApp.jsx
+++ src/Components/ChatBotApp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ChatBotApp.css"
+import "./ChatBotApp.css";
 
 const ChatBotApp = () => {
   return (
@@ -9,7 +9,7 @@ const ChatBotApp = () => {
           <h2>Chat List</h2>
           <i className='bx bx-edit-alt new-chat'></i>
         </div>
-        <div className='chat-list-item active' >
+        <div className='chat-list-item active'>
           <h4>Chat 20/07/24 12:59:42 PM</h4>
           <i className='bx bx-x circle'></i>
         </div>
@@ -21,21 +21,23 @@ const ChatBotApp = () => {
       <div className='chat-window'>
         <div className='chat-title'>
           <h3>Chat with AI</h3>
-          <i className='bx-bx-arrow-back arrow'>
-            <div className='prompt'>
-              Hi, how are you? <span>12:59:51 PM</span>
-            </div>
-            <div className='response'>
-              Hello! I'm just a computer program, so I don't have feelings, but I'm here and ready to assist you. How can I help you today?<span>12:59:52 PM</span>
-            </div>
-            <div className="typing">Typing...</div>
-            <form className="msg-form">
-              <i className="fa-solid fa-face smile emoji"></i>
-              <input type="text" className="msg-input" placeholder="Type a message..." />
-              <i className="fa-solid fa-paper-plane"></i>
-            </form>
-          </i>
+          <i className='bx bx-arrow-back arrow'></i>
         </div>
+        <div className='chat'>
+          <div className='prompt'>
+            Hi, how are you? <span>12:59:51 PM</span>
+          </div>
+          <div className='response'>
+            Hello! I'm just a computer program, so I don't have feelings, but I'm here and ready to
+            assist you. How can I help you today?<span>12:59:52 PM</span>
+          </div>
+          <div className='typing'>Typing...</div>
+        </div>
+        <form className='msg-form'>
+          <i className='fa-solid fa-face-smile emoji'></i>
+          <input type='text' className='msg-input' placeholder='Type a message...' />
+          <i className='fa-solid fa-paper-plane'></i>
+        </form>
       </div>
     </div>
   );
