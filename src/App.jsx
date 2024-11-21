9275a93a5a668e7d7c80a4203fa76a5713d3e3c4
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-21 09:20:08 -0600

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-
-import React from 'react'
-import ChatBotStart from './Components/ChatBotStart'
-import ChatBotApp from './Components/ChatBotApp';
+import React, {useState} from "react";
+import ChatBotStart from "./Components/ChatBotStart";
+import ChatBotApp from "./Components/ChatBotApp";
 
 const App = () => {
+  const [isChatting, setIsChatting] = useState(false);
+  const [ chats, setChants ] = useState( [] )
+  
+  const handleStartChat = () => {
+    setIsChatting(true);
+  };
+
+  const handleGoBack = () => {
+    setIsChatting(false);
+  };
+
   return (
     <div className='container'>
-      {/* <ChatBotStart /> */}
-      <ChatBotApp />
+      {isChatting ? (
+        <ChatBotApp onGoBack={handleGoBack} />
+      ) : (
+        <ChatBotStart onStartChat={handleStartChat} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
