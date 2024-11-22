d7c5c1649d11344039bb7bee8a101c2fa29bba53
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-22 09:18:07 -0600

    basic chat functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import ChatBotApp from "./Components/ChatBotApp";
 
 const App = () => {
   const [isChatting, setIsChatting] = useState(false);
-  const [ chats, setChants ] = useState( [] )
-  
+  const [chats, setChats] = useState([]);
+
   const handleStartChat = () => {
     setIsChatting(true);
+
+    if (chats.length === 0) {
+      const newChat = {
+        id: `Chat ${new Date().toLocaleDateString("en-GB")} ${new Date().toLocaleTimeString()}`,
+        messages: [],
+      };
+      setChats([newChat]);
+    }
   };
 
   const handleGoBack = () => {
@@ -17,7 +25,7 @@ const App = () => {
   return (
     <div className='container'>
       {isChatting ? (
-        <ChatBotApp onGoBack={handleGoBack} />
+        <ChatBotApp onGoBack={handleGoBack} chats={chats} setChats={setChats} />
       ) : (
         <ChatBotStart onStartChat={handleStartChat} />
       )}
